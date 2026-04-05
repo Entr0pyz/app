5069a1954b8ce2e4cc6da6850846540e2b8f5844
Entr0pyz <sahilgoyal5119@gmail.com>
2026-04-05 23:02:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ironlog/', // Replace 'ironlog' with your exact GitHub repo name!
+  base: '/app/', 
 })
